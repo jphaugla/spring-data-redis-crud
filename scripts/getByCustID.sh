aa02796ca440a7e722583584ccab61be66f43c95
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-03-09 15:35:56 -0500

    remove xtra files

--- scripts/getByCustID.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET -H "Content-Type: application/json"  'http://localhost:8080/customer/?customerId=1000182'
--- scripts/getByPhone.sh
+++
@@ -1,1 +0,0 @@
-curl -X GET -H "Content-Type: application/json"  'http://localhost:8080/customerByPhone/?phoneString=1000182'
