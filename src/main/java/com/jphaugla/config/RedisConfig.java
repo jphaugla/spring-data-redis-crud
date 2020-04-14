9945e9202086ec614181706ecb5f612549567bd2
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-04-13 21:19:53 -0500

    move version forward

--- src/main/java/com/jphaugla/config/RedisConfig.java
+++ src/main/java/com/jphaugla/config/RedisConfig.java
@@ -1,4 +1,20 @@
 package com.jphaugla.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
 public class RedisConfig {
+    @Value("${redis_host:localhost}")
+    private String redis_host;
+    @Value("${redis_port:6379}")
+    private int redis_port;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redis_host, redis_port);
+        return new JedisConnectionFactory(config);
+    }
 }
